b738b2b0013cf48d35d70c1258b49ac51649944f
Arjun-G-04 <arjungirirajkumar@gmail.com>
2025-12-19 22:16:26 +0530

    feat: add basic auth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import Sample from "@/components/Sample";
+import { authClient } from "@/lib/auth-client";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({ component: App });
@@ -10,6 +11,16 @@ function App() {
 				Hello World! This is Synergy 2026!
 			</span>
 			<Sample />
+			<button
+				type="button"
+				onClick={async () => {
+					await authClient.signIn.social({
+						provider: "google",
+					});
+				}}
+			>
+				Sign in with Google
+			</button>
 		</div>
 	);
 }
